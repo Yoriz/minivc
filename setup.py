7c49505a72b86338da594d21cc5a7f048c55ca6c
nathantoddstone <nathan@nathantoddstone.com>
2012-09-26 17:09:30 -0700

    "pypi"

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='MiniVC',
-    version='0.1.0',
+    name='minivc',
+    version='0.1.02',
     author='Nathan ToddStone',
     author_email='me@nathants.com',
     packages=find_packages(),
     scripts=['bin/mvc'],
-    url='http://pypi.python.org/pypi/MiniVC/',
+    url='http://pypi.python.org/pypi/minivc/',
     license='LICENSE.txt',
     description='a minimal mvc library, with zero frameworks attached.',
     long_description=open('README.rst').read(),
